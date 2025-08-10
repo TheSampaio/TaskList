5b7c2d4c98a1c865bbdc2ed126614f640e7ae268
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-08-10 19:56:44 -0300

    Create LoginEndpoints.cs
    - Added LoginEndpoints Class

---
+++ Backend/Endpoints/LoginEndpoints.cs
@@ -0,0 +1,20 @@
+﻿namespace Backend.Endpoints
+{
+    public static class LoginEndpoints
+    {
+        public static IEndpointRouteBuilder MapLoginEndpoints(this IEndpointRouteBuilder builder)
+        {
+            builder.MapGet("login", () =>
+            {
+                return "GET /login";
+            });
+
+            builder.MapPost("login", () =>
+            {
+                return "POST /login";
+            });
+
+            return builder;
+        }
+    }
+}
