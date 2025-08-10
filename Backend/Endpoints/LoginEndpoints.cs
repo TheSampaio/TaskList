30c8819a7f6cf0495b76682752fba9a2e56e9c22
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-08-10 20:23:25 -0300

    Remove GET Method From LoginEndpoints

--- Backend/Endpoints/LoginEndpoints.cs
+++ Backend/Endpoints/LoginEndpoints.cs
@@ -1,15 +1,14 @@
-﻿namespace Backend.Endpoints
+﻿using Backend.Models;
+using Dapper;
+using System.Data;
+
+namespace Backend.Endpoints
 {
     public static class LoginEndpoints
     {
         public static IEndpointRouteBuilder MapLoginEndpoints(this IEndpointRouteBuilder builder)
         {
-            builder.MapGet("login", () =>
-            {
-                return "GET /login";
-            });
-
-            builder.MapPost("login", () =>
+            builder.MapPost("login", (IDbConnection connection) =>
             {
                 return "POST /login";
             });
