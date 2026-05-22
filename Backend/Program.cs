64861ae1013758e56f335980c1a3a50f5abcfafe
Kellvyn Sampaio <64695440+TheSampaio@users.noreply.github.com>
2026-05-22 09:48:25 -0300

    Add clean architecture to the project

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,40 +1,14 @@
-using Backend.Endpoints;
-using Microsoft.Data.SqlClient;
-using System.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Register OpenAPI services and configure the database connection
 builder.Services
     .AddOpenApi()
-    .AddScoped<IDbConnection>(provider =>
-    {
-        return new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"));
-    });
-
-// Add CORS policy
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
-});
+    .AddControllers();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Task List API"));
 }
-
-// Use CORS policy
-app.UseCors("AllowFrontend");
-
-// Maps all app's endpoints
-app.MapTaskEndpoints();
 
 app.Run();
